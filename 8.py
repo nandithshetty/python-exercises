87af8d267979e4895b2a0c7765f501816c7cac9f
nandithshetty <nishanthshettyc44@gmail.com>
2026-06-17 16:24:37 +0530

    New Exercise

---
+++ 8.py
@@ -0,0 +1,10 @@
+#3. Print Even Numbers (1–20) My own code----happy
+
+for i in range (1,21):
+    if i%2==0:
+        print(i)
+
+
+#3. Print Even Numbers (1–20)
+for i in range(2,21,2):
+    print(i)
